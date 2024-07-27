eb5e2fd3671791ce747eceb4ba6cc7393dbb5160
salah <mohamednabwe53@gmail.com>
2024-07-27 18:12:29 +0300

    init commit

---
+++ src/main/java/Bots/ActionBot.java
@@ -0,0 +1,38 @@
+package Bots;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.FluentWait;
+
+import java.time.Duration;
+
+public class ActionBot {
+
+    private static WebDriver driver ;
+    static FluentWait wait = new FluentWait<>(driver)
+            .withTimeout(Duration.ofSeconds(30))
+            .pollingEvery(Duration.ofMillis(250))
+            .ignoring(NotFoundException.class)
+            .ignoring(ElementNotInteractableException.class)
+            .ignoring(AssertionError.class)
+            .ignoring(StaleElementReferenceException.class);
+
+    public static void clicking(WebDriver driver , By locator){
+        wait.until(f -> {
+            driver.findElement(locator).click();
+            return true;
+        });
+    }
+
+    public static void enterText(WebDriver driver , By locator, String text){
+        wait.until(f -> {
+            driver.findElement(locator).sendKeys(text);
+            return true;
+        });
+    }
+
+    public static void isVisible(WebDriver driver , By locator){
+        wait.until(f -> {
+            driver.findElement(locator).isDisplayed();
+            return true;
+        });
+    }
+}
---
+++ src/main/java/Pages/HomePage.java
@@ -0,0 +1,34 @@
+package Pages;
+import Bots.ActionBot;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+
+    private final WebDriver driver;
+
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+
+
+    ///// Locators /////
+
+    private final By signUpLink = By.xpath("//a[@href='/login']");
+
+    private final By silder = By.xpath("(//div[@class='carousel-inner'])[1]");
+
+
+    public HomePage silderIsVisible(){
+        ActionBot.isVisible(driver,silder);
+        return new HomePage(driver);
+    }
+
+
+//    public Login clickOnSignupLinkButton(){
+//        ActionBot.clicking(driver, signUpLink);
+//        return  new Login(driver);
+//    }
+}
